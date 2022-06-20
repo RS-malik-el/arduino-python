23236bf047083ba68cdd7d9115654944c2eae3f9
RS_Exau_Kim <92597014+RS-malik-el@users.noreply.github.com>
2022-06-20 18:15:47 +0200

    Add files via upload

---
+++ arduino&python Allumage et Clignoter des leds.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Created on Mon Dec 20th 2021
+    @author: Rachel Système
+"""
+import pyfirmata as f
+import time
+
+# "COM3" #Windows
+# "/dev/ttyACM3" # Linux
+# "/dev/tty.usbmodem11401"# Mac
+
+try:
+    arduino = f.ArduinoMega("COM4")
+    i=True
+    print("arduino connecté")
+except:
+    i=False
+    print("Arduino non connecté")
+
+#void loop()
+while i:
+        try:    
+            arduino.digital[22].write(1)
+            arduino.digital[23].write(1)
+            time.sleep(0.5)
+            arduino.digital[22].write(0)
+            arduino.digital[23].write(0)
+            time.sleep(0.5)
+        except:
+            print("Arduino non connecté")
+            arduino.exit()
+            break
+        
+        
+        
+        
+        
+        
+        
